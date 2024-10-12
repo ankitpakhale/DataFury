fd54f93767a695c9e83a0ba3a3640dceb2eef230
Ankit Pakhale <akp3067@gmail.com>
2024-10-12 09:42:51 +0530

    Modified: codebase

--- src/main.py
+++ src/main.py
@@ -9,37 +9,48 @@ import boto3
 
 load_dotenv()
 
-session = boto3.Session()
-
-s3 = session.client("s3")
-
 BUCKET_NAME: str = os.getenv("BUCKET_NAME")
 DOWNLOAD_PATH: str = os.getenv("DOWNLOAD_PATH")
 
 
-def download_files(
-    bucket_name: str, download_path: str = ".././downloaded_files"
-) -> None:
+class DownloadFiles:  # pylint: disable=too-few-public-methods
     """
-    This method download files from provided destination
+    DownloadFiles Class
     """
-    bucket_name = bucket_name.strip()
 
-    if not os.path.exists(download_path):
-        os.makedirs(download_path)
+    def __init__(self) -> None:
+        """
+        Constructor method
+        """
+        __session = boto3.Session()
+        self.__s3 = __session.client("s3")
 
-    response = s3.list_objects_v2(Bucket=bucket_name)
+    def download_files(
+        self, bucket_name: str, download_path: str = ".././downloaded_files"
+    ) -> None:
+        """
+        This method download files from provided destination
+        """
+        bucket_name = bucket_name.strip()
 
-    if "Contents" in response:
-        for obj in response["Contents"]:
-            file_key = obj["Key"]
-            file_path = os.path.join(download_path, file_key)
-            os.makedirs(os.path.dirname(file_path), exist_ok=True)
-            print(f"Downloading {file_key} to {file_path}")
-            s3.download_file(bucket_name, file_key, file_path)
-    else:
-        print(f"No files found in bucket {bucket_name}.")
+        if not os.path.exists(download_path):
+            os.makedirs(download_path)
+
+        response = self.__s3.list_objects_v2(Bucket=bucket_name)
+
+        if "Contents" in response:
+            for obj in response["Contents"]:
+                file_key = obj["Key"]
+                file_path = os.path.join(download_path, file_key)
+                os.makedirs(os.path.dirname(file_path), exist_ok=True)
+                print(f"Downloading {file_key} to {file_path}")
+                self.__s3.download_file(bucket_name, file_key, file_path)
+        else:
+            print(f"No files found in bucket {bucket_name}.")
 
 
 if __name__ == "__main__":
-    download_files(bucket_name=BUCKET_NAME, download_path=DOWNLOAD_PATH)
+    download_files_obj = DownloadFiles()
+    download_files_obj.download_files(
+        bucket_name=BUCKET_NAME, download_path=DOWNLOAD_PATH
+    )
